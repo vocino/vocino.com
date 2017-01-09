228ba5693bd374d70b371ba09da04b5288923da4
Travis Vocino <travis@vocino.com>
2017-01-09 11:46:31 +0000

    huge source move

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ gem 'jekyll'
 gem 'rake'
 gem 'html-proofer'
 
+gem 'susy'
+gem 'scss_lint'
+
 # If you want to use GitHub Pages, remove the "gem "jekyll"" above and
 # uncomment the line below. To upgrade, run `bundle update github-pages`.
 # gem "github-pages", group: :jekyll_plugins
@@ -22,5 +25,6 @@ group :jekyll_plugins do
   gem 'jekyll-feed'
   gem 'jekyll-paginate'
   gem 'jekyll-gist'
+  gem 'jekyll-seo-tag'
   gem 's3_website'
 end
---
+++ Gruntfile.js
@@ -0,0 +1,265 @@
+module.exports = function(grunt) {
+
+  'use strict';
+
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // CONFIGURABLE PATHS
+    config: {
+      source: 'source',
+      dest: 'build'
+    },
+
+
+    // GENERAL TASKS
+
+    jekyll: {
+      options: {
+        bundleExec: true,
+        config: '_config.yml,_config_prod.yml'
+      },
+      dev: {
+        options: {
+          config: '_config.yml',
+          src: '<%= config.source %>',
+          dest: '<%= config.dest %>'
+        }
+      },
+      prod: {
+        options: {
+          src: '<%= config.source %>',
+          dest: '<%= config.dest %>'
+        }
+      },
+      check: {
+        options: {
+          doctor: true
+        }
+      }
+    },
+
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : [
+            '<%= config.dest %>/*.html',
+            '<%= config.dest %>/css/*.css',
+            '<%= config.dest %>/js/*.js',
+            '<%= config.source %>/img/**/*.{jpg,png,svg,gif}'
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: '<%= config.dest %>'
+          }
+        }
+      }
+    },
+
+    watch: {
+      sass: {
+        files: '<%= config.source %>/_scss/**/*.scss',
+        tasks: ['sass', 'postcss:dev', 'penthouse']
+      },
+      jekyll: {
+        files: ['Gruntfile.js', '<%= config.source %>/**/*.{html,md,js}'],
+        tasks: ['jekyll:dev']
+      }
+    },
+
+    copy: {
+      loadCSS: {
+        files: {
+          '<%= config.source %>/_includes/loadCSS.js': 'bower_components/loadcss/loadCSS.js'
+        }
+      }
+    },
+
+
+    // USEMIN SECTION
+
+    useminPrepare: {
+      options: {
+        dest: '<%= config.dest %>',
+        // staging: '<%= config.source %>/_tmp'
+      },
+      html: '<%= config.dest %>/index.html'
+    },
+
+    usemin: {
+      options: {
+        assetsDirs: '<%= config.dest %>',
+        /* async / defer for generated JS - https://github.com/yeoman/grunt-usemin/issues/391
+        blockReplacements: {
+          js: function (block){
+            return '<script async src='' + block.dest + '' defer=defer><\/script>';
+          }
+        } */
+      },
+      html: ['<%= config.dest %>/**/*.html'],
+      // css: ['<%= config.dest %>/css/**/*.css'],
+    },
+
+    concat: { },
+
+    uglify: { },
+
+
+    // STYLESHEET SECTION
+
+    sass: {
+      options: {
+        includePaths: ['node_modules/susy/sass']
+      },
+      dist: {
+        files: {
+          '.tmp/css/style.css': '<%= config.source %>/_scss/style.scss'
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        map: false,
+        processors: [
+          require('autoprefixer')({browsers: 'last 2 versions, > 2%, ie >= 8, Firefox ESR, Opera 12.1'}),
+          require('css-mqpacker')()
+        ]
+      },
+      dev: {
+        src: '.tmp/css/style.css',
+        dest: '<%= config.dest %>/css/style.css'
+      },
+      prod: {
+        src: '.tmp/concat/css/style.css'
+      }
+    },
+
+    csscomb: {
+      dist: {
+        files: {
+          '.tmp/concat/css/style.css': '.tmp/concat/css/style.css'
+        }
+      }
+    },
+
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1,
+        // compatibility: 'ie8'
+      }
+    },
+
+    penthouse: {
+      dist: {
+        outfile : '<%= config.source %>/_includes/critical.css',
+        css : '.tmp/css/style.css',
+        url : 'http://localhost:3000',
+        width : 1280,
+        height : 800
+      }
+    },
+
+
+    // HTML SECTION
+
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          keepClosingSlash: true,
+          minifyCSS: true,
+          minifyJS: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= config.dest %>',
+          src: '**/*.html',
+          dest: '<%= config.dest %>'
+        }]
+      }
+    },
+
+    cacheBust: {
+      assets: {
+        options: {
+          length: 8,
+          deleteOriginals: true,
+          // ignorePatterns: ['.png', '.jpg', '.ico'],
+          baseDir: '<%= config.dest %>',
+          assets: ['css/*', 'js/*']
+        },
+        src: ['<%= config.dest %>/**/*.html']
+      }
+    },
+
+
+    // CODE QUALITY SECTION
+
+    scsslint: {
+      options: {
+        bundleExec: true,
+        colorizeOutput: true,
+        config: '.scss-lint.yml',
+      },
+      check: '<%= config.source %>/_scss/*.scss'
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      check: ['Gruntfile.js', '<%= config.source %>/js/*.js'],
+    },
+
+    devUpdate: {
+      check: {
+        options: {
+          reportUpdated: false,
+          updateType: 'prompt'
+        }
+      }
+    }
+
+
+  });
+
+    grunt.registerTask('dev', [
+      'sass',
+      'jekyll:dev',
+      'postcss:dev',
+      'browserSync',
+    	'watch'
+    ]);
+
+    grunt.registerTask('prod', [
+      'sass',
+      'jekyll:prod',
+      'useminPrepare',
+      'concat',
+      'postcss:prod',
+      'csscomb',
+      'cssmin',
+      'uglify',
+      'usemin',
+      'cacheBust',
+      'htmlmin'
+    ]);
+
+    grunt.registerTask('check', [
+      'jekyll:check',
+      'jshint',
+      'scsslint',
+      'devUpdate'
+    ]);
+
+  };
