4299350bbaa097f008bfee1e69fe4d4164f7aa74
Travis Vocino <travis@vocino.com>
2017-01-30 17:57:04 +0000

    gotta start somewhere

---
+++ source/_plugins/bodyClass.rb
@@ -0,0 +1,32 @@
+class BodyClassTag < Liquid::Tag
+  def generate_body_class(prefix, id)
+    id = id.gsub(/\.\w*?$/, '').gsub(/[-\/]/, '_').gsub(/^_/, '') # Remove extension from url, replace '-' and '/' with underscore, Remove leading '_'
+
+    prefix = case prefix
+             when 'class'
+               ''
+             else
+               "#{prefix}_"
+             end
+
+    "#{prefix}#{id}"
+  end
+
+  def render(context)
+    page = context.environments.first['page']
+    classes = []
+
+    %w(class url categories tags layout).each do |prop|
+      next unless page.key?(prop)
+      if page[prop].is_a?(Array)
+        page[prop].each { |proper| classes.push generate_body_class(prop, proper) }
+      else
+        classes.push generate_body_class(prop, page[prop])
+      end
+    end
+
+    classes.join(' ')
+  end
+end
+
+Liquid::Template.register_tag('body_class', BodyClassTag)
