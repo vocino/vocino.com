a59bfeec1f99f8daf4fe9b81a4968e5e66c1583f
Travis Vocino <travis@vocino.com>
2017-05-22 15:01:21 -0700

    randomize list

--- source/assets/javascripts/vocino.js
+++ source/assets/javascripts/vocino.js
@@ -1,71 +1,21 @@
 $(document).ready(function(){
-
-    var getMax = function(){
-        return $(document).height() - $(window).height();
-    }
-
-    var getValue = function(){
-        return $(window).scrollTop();
-    }
-
-    if('max' in document.createElement('progress')){
-        // Browser supports progress element
-        var progressBar = $('progress');
-
-        // Set the Max attr for the first time
-        progressBar.attr({ max: getMax() });
-
-        $(document).on('scroll', function(){
-            // On scroll only Value attr needs to be calculated
-            progressBar.attr({ value: getValue() });
-        });
-
-        $(window).resize(function(){
-            // On resize, both Max/Value attr needs to be calculated
-            progressBar.attr({ max: getMax(), value: getValue() });
-        });
-    }
-    else {
-        var progressBar = $('.progress-bar'),
-            max = getMax(),
-            value, width;
-
-        var getWidth = function(){
-            // Calculate width in percentage
-            value = getValue();
-            width = (value/max) * 100;
-            width = width + '%';
-            return width;
-        }
-
-        var setWidth = function(){
-            progressBar.css({ width: getWidth() });
-        }
-
-        $(document).on('scroll', setWidth);
-        $(window).on('resize', function(){
-            // Need to reset the Max attr
-            max = getMax();
-            setWidth();
-        });
-    }
-});
-
-// Widow titles
-$(".post-title").each(function() {
-  var wordArray = $(this).text().split(" ");
-  if (wordArray.length > 1) {
-    wordArray[wordArray.length-2] += "&nbsp;" + wordArray[wordArray.length-1];
-    wordArray.pop();
-    $(this).html(wordArray.join(" "));
-  }
-});
-
-$(".post-subtitle").each(function() {
-  var wordArray = $(this).text().split(" ");
-  if (wordArray.length > 1) {
-    wordArray[wordArray.length-2] += "&nbsp;" + wordArray[wordArray.length-1];
-    wordArray.pop();
-    $(this).html(wordArray.join(" "));
-  }
+      $('.projects').each(function(){
+            // get current ul
+            var $ul = $(this);
+            // get array of list items in current ul
+            var $liArr = $ul.children('.project');
+            // sort array of list items in current ul randomly
+            $liArr.sort(function(a,b){
+                  // Get a random number between 0 and 10
+                  var temp = parseInt( Math.random()*10 );
+                  // Get 1 or 0, whether temp is odd or even
+                  var isOddOrEven = temp%2;
+                  // Get +1 or -1, whether temp greater or smaller than 5
+                  var isPosOrNeg = temp>5 ? 1 : -1;
+                  // Return -1, 0, or +1
+                  return( isOddOrEven*isPosOrNeg );
+            })
+            // append list items to ul
+            .appendTo($ul);
+      });
 });
